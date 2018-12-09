55858a74fea7c6a49115bb60aaab6823779ed795
teja42 <shivateja5050@outlook.com>
2018-12-09 17:03:12 +0530

    add error handling & improve code

---
+++ example.cr
@@ -0,0 +1,4 @@
+require "./main.cr"
+
+cold = Cold.new
+cold.listen 3000
--- main.cr
+++ main.cr
@@ -2,37 +2,43 @@ require "http/server"
 
 class Cold
 
-   property port : Int32
+   @port : Int32
 
-   @routeInfo = Hash{
-      "get" : Hash{},
-      "post" : Hash{} 
-   }
+   # @routeInfo
+   # routeInfo is a named tuple
+   # contains proc to execute on a specific route
 
-   def initialize(port : Int32)
-      if port < 0
-         puts "Warning : Invalid port"
-         return
-      end
-      @port = port 
-      # @server
-      # path is a string to a JSON config file
+   def initialize
+      @port = 80
    end
 
-   def listen
+   def listen(port : Int32)
+      if port < 0 || port > 65_535
+         puts "Warning : Invalid port. Using 80"
+      else
+         @port = port
+      end
+
       server = HTTP::Server.new do |context|
          context.response.content_type = "application/json"
          context.response.print "{a:2}"
          # PrettyPrint.format(context.request,STDOUT,80,"\n",3)
-         puts context.request.resource
+         puts context.request.path
       end
-      server.bind_tcp @port
-      puts "Listening on port #{@port}"
-      server.listen
+
+      begin
+         server.bind_tcp @port
+      rescue ex
+         puts "Error : Failed to bind to port #{@port}. Reason {\n\t#{ex.message}\n}"
+      else
+         puts "Listening on port #{@port}"
+         server.listen
+      end
+
    end
 
-   def on(method : String , path : String , cb : proc)
-      @routeInfo[:method][:path] = cb
-   end
+   # def on(method : String , path : String , &block : )
+   #    @routeInfo[method][path] = cb
+   # end
 
 end
