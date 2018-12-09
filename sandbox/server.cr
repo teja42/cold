2646fab857aa71e523429d976cf6685dce6a702c
teja42 <shivateja5050@outlook.com>
2018-12-09 17:29:03 +0530

    Remove files from .gitignore

--- sandbox/server.cr
+++
@@ -1,7 +0,0 @@
-require "http/server"
-
-server = HTTP::Server.new(3000) do |context|
-   context.response.content_type = "text/plain"
-   context.response.print "Hello world!"
-   puts PrettyPrint.new.format(context.request)
-end
