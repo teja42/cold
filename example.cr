8b1ac401b716d3b337bdb46edcbd7db027012b19
teja42 <shivateja5050@outlook.com>
2018-12-12 01:35:46 +0530

    Tweak @handler to accept only one Proc per route

--- example.cr
+++ example.cr
@@ -6,4 +6,12 @@ cold.on "get", "/hey" { |f|
    f.send("hey there");
 }
 
+cold.on "get", "/" {|f|
+   f.send "Hello world!"
+}
+
+cold.on "post", "/" {|f|
+   f.send "Hello to post!"
+}
+
 cold.listen 3000
--- src/handler.cr
+++ src/handler.cr
@@ -4,30 +4,22 @@ module Cold
    class Handler
       include HTTP::Handler
 
-      @routeInfo : Hash(String,Hash(String, Array(Proc(Cold::Facade,Nil))))
+      @routeInfo : Hash(String,Hash(String, Proc(Cold::Facade,Nil)))
       # @routeInfo(method,path,array of handlers)
 
       def initialize
-         @routeInfo = {} of String => Hash(String, Array(Proc(Cold::Facade,Nil)))
+         @routeInfo = {} of String => Hash(String, Proc(Cold::Facade,Nil))
       end
 
       def addRoute(method : String, path : String,p : Proc(Cold::Facade,Nil))
          begin
             @routeInfo[method]
          rescue
-            @routeInfo[method] = {} of String => Array(Proc(Cold::Facade,Nil))
+            @routeInfo[method] = {} of String => Proc(Cold::Facade,Nil)
          ensure
-            begin
-               @routeInfo[method][path]
-            rescue
-               @routeInfo[method][path] = [] of Proc(Cold::Facade,Nil)
-            ensure
-               @routeInfo[method][path].push(p);
-            end
+            @routeInfo[method][path] = p
          end
       end
-
-      # def addRoute(method : String, path : String, Proc)
       
       def call(context : HTTP::Server::Context)
 
@@ -46,23 +38,18 @@ module Cold
             return
          end
 
-         routes = @routeInfo[context.request.method][context.request.resource]
+         route = @routeInfo[context.request.method][context.request.resource]
 
-         i = 0
-         while i < routes.size
-            begin
-               routes[i].call(Cold::Facade.new(context))
+         begin
+            route.call(Cold::Facade.new(context))
+         rescue ex
+            puts "Warning : Exception raised while trying to reply to : #{context.request.method} #{context.request.path}. Check log for more info"
+
+            begin # Because if the headers have already been sent then respond_with_error call would cause an error
+               context.response.respond_with_error
             rescue ex
-               puts "Warning : Exception raised while trying to reply to : #{context.request.method} #{context.request.path}. Check log for more info"
-
-               begin # Because if the headers have already been sent then respond_with_error call would cause an error
-                  context.response.respond_with_error
-               rescue ex
-               end
-
-            else
-               i = i+1
             end
+
          end
 
       end
